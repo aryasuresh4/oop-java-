5f88564d40b9a6763a60937d18280db5031a6321
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-03-20 13:55:03 +0530

    Add files via upload

---
+++ RectangleArea.java
@@ -0,0 +1,29 @@
+class Rectangle
+{
+ float length;
+ float breadth;
+ double area;
+ 
+ void setData()
+  {
+   System.out.println("variables are...");
+   length=12.48;
+   System.out.println("Length is: "+length);
+   breadth=13;
+   System.out.println("Breadth is: "+breadth);
+  }
+  
+ void area()
+ {
+   area=length*breadth;
+   System.out.println("Area of rectangle: "+area);
+ }
+
+ public static void main(String args[])
+ {
+  Rectangle r1=new Rectangle();
+  r1.setData();
+  r1.area();
+ }
+}
+
