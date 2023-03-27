fcc9b7c127ea8f13d87824817afa1e3ca0023043
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-03-27 14:47:22 +0530

    Add files via upload

---
+++ Student_marks.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Student_marks
+{
+	public static void main(String args[])
+	{
+		int n,total=0,percentage;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter no. of subjects:");
+		n=s.nextInt();
+		int marks[]=new int[n];
+		System.out.println("Enter marks out of 100:");
+		for(int i=0;i<n;i++)
+		{
+			marks[i]=s.nextInt();
+			total=total+marks[i];
+		}
+		percentage=total/n;
+		System.out.println("Total mark obtained:"+total);
+		System.out.println("Percentage:"+percentage);
+	}
+}
+
