b83eeb50c02b5947f7bdecc16c48c782db787133
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-04-03 15:47:24 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Alphabet{
+public static void main(String args[]) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("Input String Value which should be sorted : ");
+String inputString = scanner.nextLine();
+char arrayOfCharacters[] = inputString.toCharArray();
+Arrays.sort(arrayOfCharacters); 
+String sortedString = new String(arrayOfCharacters); 
+
+System.out.println("After Sorting :  " + sortedString);
+}
+}
